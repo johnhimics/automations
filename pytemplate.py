05eb21b468b33f64a0ec859af66a5d9a0a8886f1
John Himics <john@johnhimics.com>
2015-12-31 14:09:53 -0500

    Create pytemplate.py
    python starter file

---
+++ pytemplate.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""
+	TITLE:		Title
+	AUTHOR: 	John Himics
+	EMAIL: 		john@johnhimics.com
+	TIMEZONE: 	EST
+	VERSION: 	0
+	
+	DESCRIPTION:
+	
+	DEPENDANCIES:
+	
+"""
+
+#Global Variables
+
+
+#Methods
+
+
+
+#Program starts here
+if __name__ == "__main__":
+    print 'Hello and Welcome! Program running'
+    pass
+    print 'Program Complete, Goodbye!'
